3e56f1a18559b6f9c3c277b16a75edaddc1a14b5
Simran Singh <74623282+sim-singh-112@users.noreply.github.com>
2026-09-11 05:47:26 +0530

    Update and rename Node-Health to nodehealth.sh

---
+++ nodehealth.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+
+###################################
+#
+#This script outputs the node health
+#
+#####################################
+
+echo "Print the disk space "
+
+df -h
+
+echo "Print the memory "
+
+free -g
+
+echo "Print the CPU "
+
+nproc
+
+
+
